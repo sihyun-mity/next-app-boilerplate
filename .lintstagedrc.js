e98301d19eb401144970fc64f6266fd0ecc17d97
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-05-09 01:59:21 +0900

    chore: update git pre-hook

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,11 +2,12 @@
 
 const path = require('path');
 
+const compile = 'tsc-files --pretty --noEmit';
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 const prettier = 'prettier --write';
 
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand, prettier],
+  '**/*.{js,jsx,ts,tsx}': [compile, buildEslintCommand, prettier],
   '**/*.{css,scss,json,html}': prettier,
 };
