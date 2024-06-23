e78100a1f781a8f02f037c3dbd4bc5209238069a
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-06-24 01:02:33 +0900

    remove ts compile

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,12 +2,11 @@
 
 const path = require('path');
 
-const compile = 'tsc-files --pretty --noEmit';
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 const prettier = 'prettier --write';
 
 module.exports = {
-  '**/*.{js,jsx,ts,tsx}': [compile, buildEslintCommand, prettier],
+  '**/*.{js,jsx,ts,tsx}': [buildEslintCommand, prettier],
   '**/*.{css,scss,json,html}': prettier,
 };
