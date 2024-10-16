c411ba2ea3c1f4319dad90c0590156fe074f9516
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2024-10-16 21:17:32 +0900

    image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    scrollRestoration: true
+  },
+  images: {
+    formats: ['image/avif', 'image/webp']
+  }
+};
 
 export default nextConfig;
