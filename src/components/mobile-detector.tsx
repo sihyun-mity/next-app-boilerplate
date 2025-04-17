164210a4e26b24d09417db9b6506b765ae1f27c5
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2025-04-17 14:38:49 +0900

    feat: mobile detector aos, ios

--- src/components/mobile-detector.tsx
+++ src/components/mobile-detector.tsx
@@ -5,13 +5,15 @@ import MobileDetect from 'mobile-detect';
 import { useMobileStore } from '@/stores';
 
 const MobileDetector = ({ children }: { children: ReactNode }) => {
-  const { setIsMobile } = useMobileStore();
+  const { setIsMobile, setIsAndroid, setIsIOS, setIsReady } = useMobileStore();
 
   useLayoutEffect(() => {
-    const userAgent = new MobileDetect(window.navigator.userAgent),
-      isMobile = !!userAgent.mobile();
-    setIsMobile(isMobile);
-  }, [setIsMobile]);
+    const userAgent = new MobileDetect(window.navigator.userAgent);
+    setIsMobile(!!userAgent.mobile());
+    setIsAndroid(userAgent.os() === 'AndroidOS');
+    setIsIOS(userAgent.os() === 'iOS' || userAgent.os() === 'iPadOS');
+    setIsReady(true);
+  }, [setIsMobile, setIsAndroid, setIsIOS, setIsReady]);
 
   return children;
 };
