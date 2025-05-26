f3324965939be584e24e7821c754cef9c0413521
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2025-05-26 15:26:14 +0900

    fix: undo robots condition

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      ...(process.env.NODE_ENV === 'production' ? { allow: '/' } : { disallow: '/' }),
+      allow: '/',
     },
   };
 }
