81531cc4e21fddf0f29ce63ea17da1b893724fc7
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2025-06-26 15:00:30 +0900

    feat: resetQuery

--- src/hooks/useSearchQuery.ts
+++ src/hooks/useSearchQuery.ts
@@ -4,7 +4,7 @@ import { usePathname, useRouter } from 'next/navigation';
 import queryString from 'query-string';
 import { useAllSearchParams } from './index';
 
-const useSearchQuery = <T extends ParsedUrlQuery>(): [T, (query: Partial<T>) => void] => {
+const useSearchQuery = <T extends ParsedUrlQuery>(): [T, (query: Partial<T>) => void, () => void] => {
   const router = useRouter();
   const pathname = usePathname();
   const queries = useAllSearchParams();
@@ -14,7 +14,9 @@ const useSearchQuery = <T extends ParsedUrlQuery>(): [T, (query: Partial<T>) => 
       scroll: false,
     });
 
-  return [queries as T, setQuery];
+  const resetQuery = () => router.replace(pathname, { scroll: false });
+
+  return [queries as T, setQuery, resetQuery];
 };
 
 export default useSearchQuery;
