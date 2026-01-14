a1e0bdab1db2f7a1ca445dd706bb23b7bfc83236
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-01-14 10:29:15 +0900

    fix: document defined

--- src/components/portal.tsx
+++ src/components/portal.tsx
@@ -2,13 +2,14 @@
 
 import type { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
+import { IS_SERVER } from 'swr/_internal';
 
 type Props = {
   children: ReactNode;
 };
 
 const Portal = ({ children }: Props) => {
-  const node = document?.querySelector('#next-app-portal');
+  const node = IS_SERVER ? null : document.querySelector('#next-app-portal');
 
   if (node) return createPortal(children, node);
 
