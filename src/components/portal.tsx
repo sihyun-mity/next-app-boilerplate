c5f8b196e0f723d7f5d982ffb53b4a187e624538
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-01-15 15:17:50 +0900

    fix: use dynamic import to disable SSR for Portal component

--- src/components/portal.tsx
+++ src/components/portal.tsx
@@ -2,18 +2,20 @@
 
 import type { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
-import { IS_SERVER } from 'swr/_internal';
+import dynamic from 'next/dynamic';
 
 type Props = {
   children: ReactNode;
 };
 
-const Portal = ({ children }: Props) => {
-  const node = IS_SERVER ? null : document.querySelector('#next-app-portal');
+export default dynamic(
+  () =>
+    Promise.resolve(({ children }: Props) => {
+      const node = document.querySelector('#next-app-portal');
 
-  if (node) return createPortal(children, node);
+      if (node) return createPortal(children, node);
 
-  return null;
-};
-
-export default Portal;
+      return null;
+    }),
+  { ssr: false },
+);
