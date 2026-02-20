9d84aaf49b6268c96b46c22943c80ad9da5e53df
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-02-20 12:19:37 +0900

    feat: add `replace` option to `useSearchQuery` for controlling navigation behavior

--- src/hooks/useSearchQuery.ts
+++ src/hooks/useSearchQuery.ts
@@ -5,24 +5,37 @@ import queryString from 'query-string';
 import { useAllSearchParams } from './index';
 import { useCallback } from 'react';
 
-const useSearchQuery = <T extends ParsedUrlQuery>(): [T, (query: Partial<T>) => void, () => void] => {
+type UseSearchQuerySetOptions = {
+  replace?: boolean;
+};
+
+const useSearchQuery = <T extends ParsedUrlQuery>(): [
+  T,
+  (query: Partial<T>, options?: UseSearchQuerySetOptions) => void,
+  () => void,
+] => {
   const router = useRouter();
   const pathname = usePathname();
   const queries = useAllSearchParams();
 
   const setQuery = useCallback(
-    (query: Partial<T>) => {
+    (query: Partial<T>, options?: UseSearchQuerySetOptions) => {
+      const { replace = true } = { ...options };
       const mergedQuery = { ...queries, ...query };
 
       // falsy 한 값을 가진 key 삭제
       const filteredQuery = Object.fromEntries(Object.entries(mergedQuery).filter(([, value]) => Boolean(value)));
 
+      const href = `${pathname}?${queryString.stringify(filteredQuery)}`;
+
       // 쿼리 업데이트
-      router.replace(`${pathname}?${queryString.stringify(filteredQuery)}`, {
-        scroll: false,
-      });
+      if (replace) {
+        router.replace(href, { scroll: false });
+      } else {
+        router.push(href, { scroll: false });
+      }
     },
-    [pathname, queries, router],
+    [pathname, queries, router]
   );
 
   const resetQuery = useCallback(() => router.replace(pathname, { scroll: false }), [pathname, router]);
