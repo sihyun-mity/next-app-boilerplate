d7eed5697c87d45ca948d2c040261ba220a17d39
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-03-20 22:12:50 +0900

    chore: remove unused experimental flags and cache configuration

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,8 @@ import { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     scrollRestoration: true,
-    cachedNavigations: true,
-    appNewScrollHandler: true,
   },
   reactCompiler: true,
-  cacheComponents: true,
   images: {
     formats: ['image/avif', 'image/webp'],
     qualities: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
