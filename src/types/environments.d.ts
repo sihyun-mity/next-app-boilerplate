2b9a6850c5e138149593a2f43079a8cd9ebcd40e
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-03-23 17:57:31 +0900

    chore: add TypeScript type declarations for NodeJS environment variables and global window object

---
+++ src/types/environments.d.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+  /** Environment variables for the application */
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface ProcessEnv {}
+}
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+declare global {
+  /** Global window object */
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface Window {}
+}
+
+export {};
