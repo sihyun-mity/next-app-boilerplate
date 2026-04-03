72ccd9fe726ed7af8bcb6e614eb9f546a5d8b27d
Sihyeon Hong <72115242+sihyun-mity@users.noreply.github.com>
2026-04-03 17:53:04 +0900

    refactor: simplify `useServerNow` logic

--- src/hooks/use-server-now.ts
+++ src/hooks/use-server-now.ts
@@ -2,7 +2,7 @@
 
 import useSWR from 'swr';
 import { getServerTime } from '@/actions';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Props = {
   autoUpdate?: boolean;
@@ -14,36 +14,34 @@ type Props = {
  * @param {Props} props - Properties
  * @param {Props['autoUpdate']} [props.autoUpdate = true] - 서버 시간을 매초마다 갱신
  */
-export function useServerNow(props?: Readonly<Props>) {
+export function useServerNow(props?: Props) {
   const { autoUpdate = true } = { ...props };
   const [now, setNow] = useState<number>();
-  const [offset, setOffset] = useState<number>(0);
   const { data, isLoading, isValidating, mutate, error } = useSWR('use-server-now', getServerTime, {
     dedupingInterval: 1000,
-    focusThrottleInterval: 1000,
+    revalidateOnFocus: false,
     keepPreviousData: true,
-    onSuccess: (data) => {
-      calculateOffset(data);
-      calculateNow();
-    },
   });
 
-  const calculateOffset = (data: number) => setOffset(data - Date.now());
-
-  const calculateNow = useCallback(() => {
-    if (!data) return;
-    setNow(Date.now() + offset);
-  }, [data, offset]);
-
   useEffect(() => {
-    if (!autoUpdate) {
-      calculateNow();
-      return;
-    }
+    if (!data) return;
 
-    const timer = setInterval(calculateNow, 1000);
+    /** 로컬 시간과 오차 계산 */
+    const offset = data - Date.now();
+
+    /** 현재 서버시간 계산 */
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setNow(Date.now() + offset);
+
+    if (!autoUpdate) return;
+
+    /** 매초마다 자동 갱신 */
+    const timer = setInterval(() => {
+      setNow(Date.now() + offset);
+    }, 1000);
+
     return () => clearInterval(timer);
-  }, [calculateNow, autoUpdate]);
+  }, [data, autoUpdate]);
 
   return { data, now, isLoading, isValidating, mutate, error };
 }
